e90741318a25836e699e407013739e7e4b6990fb
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-09-26 18:15:25 -0500

    deleted unused pages and removed from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,7 @@
 import NavBar from './components/NavBar/';
-import Sidebar from './components/Sidebar/';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./pages/Main"
 import './App.css';
-import UserGroup from './pages/UserGroup';
-import Submit from './pages/Submit';
 
 function App() {
   return (
@@ -12,8 +9,6 @@ function App() {
       <Router>
         <NavBar />
         <Route exact path="/" component={Main} />
-        <Route exact path="/userGroup" component={UserGroup} />
-        <Route exact path="/submit" component={Submit} />
       </Router>
     </div>
   );
