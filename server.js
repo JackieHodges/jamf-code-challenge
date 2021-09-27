90322d759844bf125a3acbf92d5a5645a12e4eba
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-09-27 13:55:59 -0500

    added script about heroku postbuild

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
-
-app.get("/", function (req, res) {
-    res.json(path.join(__dirname, "client/public/index.html"));
-});
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
